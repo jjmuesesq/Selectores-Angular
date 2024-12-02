c37f923b86fe27c76406ee9183ba72f3c5d34903
jjmuesesq <jjmuesesq@unal.edu.co>
2024-12-02 16:04:33 -0500

    :sparkles: Obtener paises por region - consumo endpoint

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Region, SmallCountry } from '../interfaces/country.interfaces';
+import { Observable, of, tap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
     providedIn: 'root'
@@ -7,15 +9,25 @@ import { Region, SmallCountry } from '../interfaces/country.interfaces';
 
 export class CountriesService{
 
+    private baseUrl: string = 'https://restcountries.com/v3.1';
+    //'https://restcountries.com/v3.1/region/americas?fields=cca3,name,borders';
+
     private _regions: Region[] = [ Region.Africa, Region.Americas, Region.Asia, Region.Europe, Region.Oceania ];
 
-    constructor (){}
+    constructor(
+        private http: HttpClient
+    ){}
 
     get regions(): Region[]{
         return [ ...this._regions ];
     }
 
-    getCountriesByRegion( region: Region): SmallCountry[]{
-        return [];
+    getCountriesByRegion( region: Region): Observable<SmallCountry[]>{
+        if(!region) return of([]);
+        const url: string = `${this.baseUrl}/region/${region}?fields=cca3,name,borders`;
+        return this.http.get<SmallCountry[]>(url)
+        .pipe(
+            tap(response => console.log('response =>', { response }))
+        );
     }
 }
